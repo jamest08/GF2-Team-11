adcd28e0fab6c4e2f4606c2d7d5b129194373eed
annamillsy <asm87@cam.ac.uk>
2021-05-26 15:59:08 +0100

    change to circuit diagram

--- logsim/test_parse.py
+++ logsim/test_parse.py
@@ -11,16 +11,25 @@ from network import Network
 from monitors import Monitors
 from parse import Parser
 
+
+@pytest.mark.parametrize("example", [
+    ("example1.txt"),
+    ("example1_with_syntax_errors.txt"),
+    ("example2_with_syntax_errors.txt"),
+    ("example3.txt"),
+    ("example4.txt")
+])
+
 @pytest.fixture
-def parser():
+def parser(example):
     """Return an instance of a scanner using ."""
     print("\nNow opening file...")
 
     # Print the path provided and try to open the file for reading
     cwd = os.getcwd()
-    example = "Example.txt"
-
-    path = cwd + '/' + example
+    #example = "Example.txt"
+    for file in example:
+        path = cwd + '//' + example
     print(path)
     
     names = Names()
@@ -29,8 +38,7 @@ def parser():
     network = Network(names, devices)
     monitors = Monitors(names, devices, network)
     parser = Parser(names, devices, network, monitors, scanner)
-    return parser
-
+    return parser1,
 
 
 
